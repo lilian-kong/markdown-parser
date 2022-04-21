bad50c81e40a47915c74f7d6596d2d6a384b5696
lilian-kong <llkong@ucsd.edu>
2022-04-21 16:54:00 -0700

    javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest- core-1.3.jar MarkdownParseTest.java
    java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-
    core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest

---
+++ MarkdownParseTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*; //imports library
+import org.junit.*; //imports library
+public class MarkdownParseTest {
+    @Test //tells java this is a test
+    public void addition() {
+        assertEquals(2, 1 + 1); //asserts that the two paramerters should be equal (error otherwise)
+    }
+}
