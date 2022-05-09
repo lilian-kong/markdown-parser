ec9bed62b665910dac6b84b662308a971657f8a3
lilian-kong <103224846+lilian-kong@users.noreply.github.com>
2022-05-08 20:27:47 -0700

    committing from command line

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -43,7 +43,6 @@ public class MarkdownParse {
     }
 
     public static void main(String[] args) throws IOException {
-        int i = 0;
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
