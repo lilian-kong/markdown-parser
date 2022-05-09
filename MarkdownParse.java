55d61987a61b77f2cf267ac32a61be0cbded4323
lilian-kong <103224846+lilian-kong@users.noreply.github.com>
2022-05-08 20:24:04 -0700

    testing for labreport 3

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -43,6 +43,7 @@ public class MarkdownParse {
     }
 
     public static void main(String[] args) throws IOException {
+        int i = 0;
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
